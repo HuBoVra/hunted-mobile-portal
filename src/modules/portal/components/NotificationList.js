0825831582f0ae7993a122977f5c1c15a96cf331
bras1223 <l.hermans98@gmail.com>
2017-09-15 14:17:24 +0200

    Passing on props, so List gets filled.

--- src/modules/portal/components/NotificationList.js
+++ src/modules/portal/components/NotificationList.js
@@ -4,12 +4,31 @@ import Notification from "./Notification";
 const NotificationList = () => {
   return (
     <div>
-      <Notification />
-      <Notification />
-      <Notification />
-      <Notification />
-      <Notification />
-      <Notification />
+      <Notification
+        notificationTitle="Een nieuwe locatie"
+        notificationText="Jullie nieuwe locatie ligt bij het hoogste punt van Weert"
+        time="22:09"
+      />
+      <Notification
+        notificationTitle="Jullie zijn gespot!"
+        notificationText="Spot-team 1 heeft jullie gespot! Jullie tijd zal verhoogd worden."
+        time="22:03"
+      />
+      <Notification
+        notificationTitle="Locatie bereikt"
+        notificationText="Goedzo! Jullie hebben de volgende locatie bereikt. Jullie volgende hint: 'H'. Deze hint wordt aan jullie samenvatting toegevoegd."
+        time="21:36"
+      />
+      <Notification
+        notificationTitle="Jullie 1e locatie"
+        notificationText="De eerste locatie die jullie zullen moeten bereiken is aangegeven in het kaartje hierboven."
+        time="19:12"
+      />
+      <Notification
+        notificationTitle="Let the games begin!"
+        notificationText="Welkom in het Mobile Portal! Hier ontvang je belangerijke berichten om het extractiepunt te bereiken. Kijk maar eens rond en wacht op je eerste aanwijzing."
+        time="19:00"
+      />
     </div>
   );
 };
